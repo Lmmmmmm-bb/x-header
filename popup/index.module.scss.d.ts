d0d0f3587451f0ec8a809b2deab4fe605d28fed0
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-12-07 00:13:28 +0800

    chore: init tabs

--- popup/index.module.scss.d.ts
+++ popup/index.module.scss.d.ts
@@ -1,1 +1,2 @@
+export const extraContent: string;
 export const wrapper: string;
--- popup/index.tsx
+++ popup/index.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from 'react';
-import { TabPane, Tabs } from '@douyinfe/semi-ui';
+import { IconPlus } from '@douyinfe/semi-icons';
+import { Button, TabPane, Tabs } from '@douyinfe/semi-ui';
 
 import styles from './index.module.scss';
 
@@ -13,20 +14,31 @@ const Popup: FC = () => {
   return (
     <div className={styles.wrapper}>
       <Tabs
+        type='card'
         tabPosition='left'
         activeKey={`${activeKey}`}
+        tabBarExtraContent={
+          <div className={styles.extraContent}>
+            <Button
+              block
+              size='small'
+              theme='borderless'
+              icon={<IconPlus size='small' />}
+            />
+          </div>
+        }
         onChange={handleTabChange}
       >
-        <TabPane tab='文档' itemKey='1'>
+        <TabPane tab='文档' itemKey='1' closable>
           文档
         </TabPane>
-        <TabPane tab='帮助' itemKey='2'>
+        <TabPane tab='帮助' itemKey='2' closable>
           帮助
         </TabPane>
-        <TabPane tab='快速起步' itemKey='3'>
+        <TabPane tab='快速起步' itemKey='3' closable>
           快速起步
         </TabPane>
-        <TabPane tab='测试' itemKey='4'>
+        <TabPane tab='测试' itemKey='4' closable>
           测试
         </TabPane>
       </Tabs>
